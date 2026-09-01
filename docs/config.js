25151908a16b74a24262be584ff72ae7b5485fe4
AniketJoglekar <AniketJoglekar@users.noreply.github.com>
2026-09-02 02:39:14 +0530

    Add files via upload

--- docs/config.js
+++ docs/config.js
@@ -1,10 +1,10 @@
 /* Fill both values in after deploying the Apps Script web app. */
 var CONFIG = {
   // Deploy > New deployment > Web app. Paste the URL ending in /exec.
-  API_URL: 'https://script.google.com/macros/s/AKfycbyqtnTJ4J7WN_RIS61ituN0P8VIKhH2KsvXMD4tmhKeMd-NHheGXIVbQZ-lYICBLHrd/exec',
+  API_URL: 'https://script.google.com/macros/s/PASTE_DEPLOYMENT_ID/exec',
 
   // Same OAuth 2.0 Web client ID you put in Code.gs.
-  CLIENT_ID: '813055517806-kasp2kmmq7o4qk2fks27qht4pfn4u6ps.apps.googleusercontent.com',
+  CLIENT_ID: 'PASTE_YOUR_OAUTH_WEB_CLIENT_ID.apps.googleusercontent.com',
 
   TIMEZONE: 'Asia/Kolkata'
 };
