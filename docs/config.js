6c153d33a8d2d454ba3545cc6c66895dc711362f
AniketJoglekar <AniketJoglekar@users.noreply.github.com>
2026-09-19 18:21:57 +0530

    fff
    ffff

--- docs/config.js
+++ docs/config.js
@@ -1,7 +1,7 @@
 /* Fill both values in after deploying the Apps Script web app. */
 var CONFIG = {
   // Deploy > New deployment > Web app. Paste the URL ending in /exec.
-  API_URL: 'https://script.google.com/macros/s/AKfycbxDZ_lJvemi4ZUqqKDHU5jJNfPaFJI5tcQyKn1nmwzpgmxLtU1tfMAjNbS582GqSgI8/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycby-khWWoapVbbb6LDi7P--GDgoEmik7LNi673LnMIQuxmICwWJng8dXDUrH6J7BGhbxSQ/exec',
  
   // Same OAuth 2.0 Web client ID you put in Code.gs.
   CLIENT_ID: '813055517806-kasp2kmmq7o4qk2fks27qht4pfn4u6ps.apps.googleusercontent.com',
